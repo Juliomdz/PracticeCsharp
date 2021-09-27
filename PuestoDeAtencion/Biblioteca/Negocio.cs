661d2e54bf7394b6be45e0940e467e5b0221321e
Juliomdz <julio.mdz@hotmail.com>
2021-09-27 12:30:49 -0300

    practica herencia y polimorfismo

---
+++ PuestoDeAtencion/Biblioteca/Negocio.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Negocio
+    {
+        private PuestoAtencion caja;
+        private Queue<Cliente> clientes;
+        private string nombre;
+
+        private Negocio()
+        {
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+        }
+        public Negocio(string nombre)
+            : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public Cliente Cliente
+        {
+            get { return clientes.Dequeue(); }
+            set { bool respuesta = this+value; }
+        }
+
+
+
+        public static bool operator ==(Negocio neg, Cliente cli)
+        {
+            foreach (Cliente item in neg.clientes)
+            {
+                if (cli == item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Negocio neg, Cliente cli)
+        {
+            return !(neg == cli);
+        }
+        public static bool operator +(Negocio neg, Cliente cli)
+        {
+            if (neg != cli)
+            {
+                neg.clientes.Enqueue(cli);
+                return true;
+            }
+            return false;
+        }
+        public static bool operator ~ (Negocio neg)
+        {
+            if (neg.clientes.Count > 0)
+            {
+                return neg.caja.Atender(neg.Cliente);
+            }
+            return false;
+        }
+        public int ClientesPendientes
+        {
+            get
+            {
+                return this.clientes.Count;
+            }
+        }
+    }
+
+}
---
+++ PuestoDeAtencion/Biblioteca/PuestoAtencion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class PuestoAtencion
+    {
+        public enum Puesto
+        {
+            Caja1,Caja2
+        }
+        static int numeroActual;
+        Puesto puesto;
+
+        static PuestoAtencion()
+        {
+            PuestoAtencion.numeroActual = 0;
+        }
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+        public static int NumeroActual
+        {
+            get 
+            {
+                return ++numeroActual;
+            }
+        }
+        public bool Atender(Cliente cli)
+        {
+            if (!(cli is null) && cli.Numero >=0)
+            {
+                Thread.Sleep(3000);
+                return true;
+            }
+            return false;
+        }
+    }
+}
